190c27c180074d67c1a40a4c3ea51ab8b22859eb
Aktishch <aktishch@gmail.com>
2026-05-17 18:52:45 +0300

    add include in lodash

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,48 @@
 import CopyPlugin from 'copy-webpack-plugin'
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
 import fs from 'fs'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import HtmlWebpackPlugin, { type ProcessedOptions } from 'html-webpack-plugin'
+import lodash from 'lodash'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import path from 'path'
 import TerserPlugin from 'terser-webpack-plugin'
 import { fileURLToPath } from 'url'
-import type { Configuration as WebpackConfiguration } from 'webpack'
+import type { Compilation, InputFileSystem, Configuration as WebpackConfiguration } from 'webpack'
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 
 interface Configuration extends WebpackConfiguration {
   devServer?: DevServerConfiguration
 }
 
+interface WebpackEnv {
+  production?: boolean
+  development?: boolean
+  [key: string]: string | boolean | undefined
+}
+
+interface WebpackArgv {
+  mode?: 'production' | 'development'
+  [key: string]: unknown
+}
+
 interface HtmlPlugin {
   templateDir: string
   script: 'body' | 'head' | boolean
   src: string
+  isProd: boolean
+}
+
+type LodashInclude = lodash.TemplateExecutor | undefined
+
+type Include = (targetPath: string) => LodashInclude
+
+interface TemplateParameters extends HtmlWebpackPlugin.TemplateParameter {
+  include: Include
 }
 
 const dirName: string = path.dirname(fileURLToPath(import.meta.url))
 
-const generatePlugins = ({ templateDir, script, src }: HtmlPlugin): HtmlWebpackPlugin[] => {
+const generatePlugins = ({ templateDir, script, src, isProd }: HtmlPlugin): HtmlWebpackPlugin[] => {
   const fullPath: string = path.resolve(dirName, templateDir)
 
   if (!fs.existsSync(fullPath)) return []
@@ -38,101 +59,142 @@ const generatePlugins = ({ templateDir, script, src }: HtmlPlugin): HtmlWebpackP
         scriptLoading: 'blocking',
         filename: `${src}${name}.html`,
         template: path.resolve(fullPath, templateFile),
-        minify: {
-          collapseWhitespace: false
+        minify: isProd ? { collapseWhitespace: false } : false,
+        templateParameters: (
+          compilation: Compilation,
+          assets: HtmlWebpackPlugin.TemplateParameter['htmlWebpackPlugin']['files'],
+          assetTags: HtmlWebpackPlugin.TemplateParameter['htmlWebpackPlugin']['tags'],
+          options: ProcessedOptions
+        ): TemplateParameters => {
+          const include = (targetPath: string): LodashInclude => {
+            const fileSystem: InputFileSystem | null = compilation.compiler.inputFileSystem
+
+            if (!fileSystem) return
+
+            const readFileSync: typeof fileSystem.readFileSync = fileSystem.readFileSync
+            const source: string = readFileSync ? readFileSync(path.resolve(dirName, 'src', targetPath), 'utf8') : ''
+
+            return lodash.template(source)
+          }
+
+          ;(global as typeof globalThis & { include: Include }).include = include
+
+          return {
+            compilation,
+            webpackConfig: options,
+            htmlWebpackPlugin: { tags: assetTags, files: assets, options },
+            include
+          }
         }
       })
     })
 }
 
-export default {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: path.resolve(dirName, 'src/webpack.ts'),
-  resolve: {
-    extensions: ['.js', '.ts'],
-    alias: {
-      '@plugins': path.resolve(dirName, 'plugins'),
-      '@ts': path.resolve(dirName, 'src/ts'),
-      '@utils': path.resolve(dirName, 'src/ts/utils')
-    }
-  },
-  output: {
-    path: path.resolve(dirName, 'dist'),
-    filename: 'js/application.js',
-    clean: true
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
-    ...generatePlugins({ templateDir: 'src', script: 'head', src: '' }),
-    ...generatePlugins({
-      templateDir: 'src/dialogs',
-      script: false,
-      src: 'dialogs/'
-    }),
-    new CopyPlugin({
-      patterns: [
+export default (env: WebpackEnv, argv: WebpackArgv): Configuration => {
+  const isProd: boolean = argv.mode === 'production' || env.production === true
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
+    entry: path.resolve(dirName, 'src/webpack.ts'),
+    cache: {
+      type: 'filesystem'
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+      alias: {
+        '@plugins': path.resolve(dirName, 'plugins'),
+        '@ts': path.resolve(dirName, 'src/ts'),
+        '@utils': path.resolve(dirName, 'src/ts/utils')
+      }
+    },
+    output: {
+      path: path.resolve(dirName, 'dist'),
+      filename: 'js/application.js',
+      clean: true
+    },
+    optimization: {
+      minimize: isProd,
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+      ...generatePlugins({ templateDir: 'src', script: 'head', src: '', isProd }),
+      ...generatePlugins({
+        templateDir: 'src/dialogs',
+        script: false,
+        src: 'dialogs/',
+        isProd
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: 'src/img/',
+            to: 'img/',
+            noErrorOnMissing: true
+          }
+        ]
+      })
+    ],
+    module: {
+      rules: [
         {
-          from: 'src/img/',
-          to: 'img/',
-          noErrorOnMissing: true
-        }
-      ]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        include: [path.resolve(dirName, 'src/includes'), path.resolve(dirName, 'src/components')],
-        use: ['raw-loader']
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      },
-      {
-        test: /\.m?[jt]s$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript']
+          test: /\.html$/i,
+          include: [path.resolve(dirName, 'src/includes'), path.resolve(dirName, 'src/components')],
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                sources: false,
+                esModule: false
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        },
+        {
+          test: /\.m?[jt]s$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['@babel/preset-env', '@babel/preset-typescript']
+            }
+          }
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'img/pictures/[name][ext]'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]'
           }
         }
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'img/pictures/[name][ext]'
-        }
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]'
-        }
+      ]
+    },
+    devServer: {
+      port: 9000,
+      compress: true,
+      hot: true,
+      historyApiFallback: true,
+      watchFiles: ['src/**/*.html'],
+      static: {
+        directory: path.join(dirName, 'dist')
       }
-    ]
-  },
-  devServer: {
-    port: 9000,
-    compress: false,
-    hot: true,
-    historyApiFallback: true,
-    watchFiles: ['src/**/*.html'],
-    static: {
-      directory: path.join(dirName, 'dist')
     }
-  }
-} satisfies Configuration
+  } satisfies Configuration
+}
